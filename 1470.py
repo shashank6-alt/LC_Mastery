629cc96ac3d5ba408ed31f7249c40ff2388ab5fa
D,Shashank <shashankdumpala6@gmail.com>
2026-02-22 13:43:28 +0530

    LC_1470

---
+++ 1470.py
@@ -0,0 +1,11 @@
+#leetcode 1470. Shuffle the Array
+
+# class Solution:
+#     def shuffle(self, nums: List[int], n: int) -> List[int]:      
+#         ans = []
+#         for i in range (n):
+#             ans.append(nums[i])
+#             ans.append(nums[i+n])
+
+#         return ans
+        
