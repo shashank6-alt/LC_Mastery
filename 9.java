8d8e57b25a44a911bfb0e391cbd361007b37ad78
D,Shashank <shashankdumpala6@gmail.com>
2026-02-26 22:15:38 +0530

    LC_9

---
+++ 9.java
@@ -0,0 +1,14 @@
+// Leetcode 9 . Palindrome Number
+// class Solution {
+//     public boolean isPalindrome(int x) {
+//        int temp=x;
+//         int rev = 0;
+//         while(x>0)
+//         {
+//             int digit= x%10;
+//             rev = (rev*10)+digit;
+//             x=x/10;
+//         }
+//         return rev==temp;
+//     }
+// }
