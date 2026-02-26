87c943ec771baf8cfeb67687f2c427c533838748
D,Shashank <shashankdumpala6@gmail.com>
2026-02-26 22:18:01 +0530

    LC_912

---
+++ 912.java
@@ -0,0 +1,10 @@
+//leetcode 912. Sort an Array
+
+// import java.util.Arrays;
+
+// class Solution {
+//     public int[] sortArray(int[] nums) {
+//         Arrays.sort(nums);
+//         return nums;
+//     }
+// }
