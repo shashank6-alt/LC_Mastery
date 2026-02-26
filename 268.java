664e37655b38fd902372b91ad819006f41970c68
D,Shashank <shashankdumpala6@gmail.com>
2026-02-26 22:16:57 +0530

    LC_268

---
+++ 268.java
@@ -0,0 +1,13 @@
+// leetcode 268. Missing Number
+// class Solution {
+//     public int missingNumber(int[] nums) {
+//        int n = nums.length  ;
+//        int sum = n*(n+1)/2 ;
+//        int exp = 0 ; 
+//        for(int num : nums){
+//          exp += num ;
+//        }
+
+//     return sum - exp ;
+//     }
+// }
